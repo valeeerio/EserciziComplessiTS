9a9404247a7107aa49c856b1aea82e02f7729358
valeeerio <valeriomortella03@gmail.com>
2023-12-05 12:36:21 +0100

    switch

--- RobotSimulator/app.js
+++ RobotSimulator/app.js
@@ -1,30 +1,72 @@
 var Robot = /** @class */ (function () {
-    function Robot(id, movimenti) {
+    function Robot(id, direzione, movimenti) {
         this.id = id;
-        this.posizione = { avanti: 0, destra: 0, sinistra: 0 };
+        this.direzione = direzione;
+        this.posizione = { x: 0, y: 0 };
         this.movimenti = movimenti;
     }
     Robot.prototype.eseguiMovimenti = function () {
         for (var _i = 0, _a = this.movimenti; _i < _a.length; _i++) {
-            var direzione = _a[_i];
-            this.muovi(direzione.toUpperCase());
+            var movimento = _a[_i];
+            this.muovi(movimento.toUpperCase(), this.direzione.toUpperCase());
         }
-        console.log("Posizione Finale", this.posizione);
+        console.log("Robot ".concat(this.id, ": Direzione: \"").concat(this.direzione, "\"\nPosizione Finale"), this.posizione);
     };
-    Robot.prototype.muovi = function (direzione) {
-        switch (direzione) {
-            case "A":
-                this.posizione.avanti++;
-                break;
-            case "D":
-                this.posizione.destra++;
-                break;
-            case "S":
-                this.posizione.sinistra++;
-                break;
+    Robot.prototype.muovi = function (movimento, direzione) {
+        if (direzione === "N") {
+            switch (movimento) {
+                case "A":
+                    this.posizione.y++;
+                    break;
+                case "R":
+                    this.direzione = "E";
+                    break;
+                case "L":
+                    this.direzione = "O";
+                    break;
+            }
+        }
+        else if (direzione === "E") {
+            switch (movimento) {
+                case "A":
+                    this.posizione.x++;
+                    break;
+                case "R":
+                    this.direzione = "S";
+                    break;
+                case "L":
+                    this.direzione = "N";
+                    break;
+            }
+        }
+        else if (direzione === "S") {
+            switch (movimento) {
+                case "A":
+                    this.posizione.y--;
+                    break;
+                case "R":
+                    this.direzione = "O";
+                    break;
+                case "L":
+                    this.direzione = "E";
+                    break;
+            }
+        }
+        else if (direzione === "O") {
+            switch (movimento) {
+                case "A":
+                    this.posizione.x--;
+                    break;
+                case "R":
+                    this.direzione = "N";
+                    break;
+                case "L":
+                    this.direzione = "S";
+                    break;
+            }
         }
     };
     return Robot;
 }());
-var prova = new Robot(1, "adsaas");
-prova.eseguiMovimenti();
+var robot1 = new Robot(1, "N", "RAALAL");
+robot1.eseguiMovimenti();
--- RobotSimulator/app.ts
+++ RobotSimulator/app.ts
@@ -1,33 +1,78 @@
 class Robot {
-  public posizione: { avanti: number; destra: number; sinistra: number };
+  public posizione: { x: number; y: number };
   private movimenti: string;
 
-  constructor(private id: number, movimenti: string) {
-    this.posizione = { avanti: 0, destra: 0, sinistra: 0 };
+  constructor(
+    private id: number,
+    private direzione: string,
+    movimenti: string
+  ) {
+    this.posizione = { x: 0, y: 0 };
     this.movimenti = movimenti;
   }
 
   eseguiMovimenti() {
-    for (const direzione of this.movimenti) {
-      this.muovi(direzione.toUpperCase());
+    for (const movimento of this.movimenti) {
+      this.muovi(movimento.toUpperCase(), this.direzione.toUpperCase());
     }
-    console.log("Posizione Finale", this.posizione);
+    console.log(
+      `Robot ${this.id}: Direzione: "${this.direzione}"\nPosizione Finale`,
+      this.posizione
+    );
   }
 
-  private muovi(direzione: string) {
-    switch (direzione) {
-      case "A":
-        this.posizione.avanti++;
-        break;
-      case "D":
-        this.posizione.destra++;
-        break;
-      case "S":
-        this.posizione.sinistra++;
-        break;
+  private muovi(movimento: string, direzione: string) {
+    if (direzione === "N") {
+      switch (movimento) {
+        case "A":
+          this.posizione.y++;
+          break;
+        case "R":
+          this.direzione = "E";
+          break;
+        case "L":
+          this.direzione = "O";
+          break;
+      }
+    } else if (direzione === "E") {
+      switch (movimento) {
+        case "A":
+          this.posizione.x++;
+          break;
+        case "R":
+          this.direzione = "S";
+          break;
+        case "L":
+          this.direzione = "N";
+          break;
+      }
+    } else if (direzione === "S") {
+      switch (movimento) {
+        case "A":
+          this.posizione.y--;
+          break;
+        case "R":
+          this.direzione = "O";
+          break;
+        case "L":
+          this.direzione = "E";
+          break;
+      }
+    } else if (direzione === "O") {
+      switch (movimento) {
+        case "A":
+          this.posizione.x--;
+          break;
+        case "R":
+          this.direzione = "N";
+          break;
+        case "L":
+          this.direzione = "S";
+          break;
+      }
     }
   }
 }
 
-const prova = new Robot(1, "adsaas");
-prova.eseguiMovimenti();
+const robot1 = new Robot(1, "N", "RAALAL");
+robot1.eseguiMovimenti();
